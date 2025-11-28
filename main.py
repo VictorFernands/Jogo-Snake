3cb61b79b5c40fdbd9a349545ab7d3885708f552
VictorFernands <victor.nunes.fernands@gmail.com>
2025-11-28 02:31:07 -0300

    2

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ import os
 import configparser
 import random
 
+CONFIG_FILE = os.path.join('conf', 'conf.ini')
+config = configparser.ConfigParser()
+controls = {}
+SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN = 800, 600, False
+
