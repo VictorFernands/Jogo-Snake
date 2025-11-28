670e650226cf314791229d5199b299ed8d855aac
VictorFernands <victor.nunes.fernands@gmail.com>
2025-11-28 02:33:33 -0300

    6

--- main.py
+++ main.py
@@ -33,3 +33,28 @@ direction = 'RIGHT'
 block_size = 20
 food = (random.randint(0, SCREEN_WIDTH // block_size - 1) * block_size,
         random.randint(0, SCREEN_HEIGHT // block_size - 1) * block_size)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == controls['PAUSE']:
+                running = False
+            elif event.key == controls['UP'] and direction != 'DOWN':
+                direction = 'UP'
+            elif event.key == controls['DOWN'] and direction != 'UP':
+                direction = 'DOWN'
+            elif event.key == controls['LEFT'] and direction != 'RIGHT':
+                direction = 'LEFT'
+            elif event.key == controls['RIGHT'] and direction != 'LEFT':
+                direction = 'RIGHT'
+
+    head_x, head_y = snake[0]
+    if direction == 'UP': head_y -= block_size
+    if direction == 'DOWN': head_y += block_size
+    if direction == 'LEFT': head_x -= block_size
+    if direction == 'RIGHT': head_x += block_size
+
+    
