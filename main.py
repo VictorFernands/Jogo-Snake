ef39b99da41ccf3cd3b2978673c56b48fcd8833e
VictorFernands <victor.nunes.fernands@gmail.com>
2025-11-28 02:31:55 -0300

    3

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ config = configparser.ConfigParser()
 controls = {}
 SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN = 800, 600, False
 
+try:
+    config.read(CONFIG_FILE)
+    SCREEN_WIDTH = config.getint('Display', 'width', fallback=800)
+    SCREEN_HEIGHT = config.getint('Display', 'height', fallback=600)
+    FULLSCREEN = config.getboolean('Display', 'fullscreen', fallback=False)
+    controls['UP'] = pygame.key.key_code(config.get('Controls', 'up', fallback='w'))
+    controls['DOWN'] = pygame.key.key_code(config.get('Controls', 'down', fallback='s'))
+    controls['LEFT'] = pygame.key.key_code(config.get('Controls', 'left', fallback='a'))
+    controls['RIGHT'] = pygame.key.key_code(config.get('Controls', 'right', fallback='d'))
+    controls['PAUSE'] = pygame.key.key_code(config.get('Controls', 'pause', fallback='enter'))
+except:
+    controls = {'UP': pygame.K_w, 'DOWN': pygame.K_s, 'LEFT': pygame.K_a, 'RIGHT': pygame.K_d, 'PAUSE': pygame.K_RETURN}
